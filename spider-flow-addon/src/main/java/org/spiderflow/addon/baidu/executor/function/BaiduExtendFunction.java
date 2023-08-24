978029f8f3802f23726cc421e07392f2d832b454
wangzhaoming <wangzhaoming1991@gmail.com>
2023-08-24 15:03:42 +0800

    添加方法说明

--- spider-flow-addon/src/main/java/org/spiderflow/addon/baidu/executor/function/BaiduExtendFunction.java
+++ spider-flow-addon/src/main/java/org/spiderflow/addon/baidu/executor/function/BaiduExtendFunction.java
@@ -5,11 +5,14 @@ import org.apache.commons.lang3.time.DateUtils;
 import org.spiderflow.annotation.Comment;
 import org.spiderflow.annotation.Example;
 import org.spiderflow.executor.FunctionExecutor;
+import org.springframework.stereotype.Component;
 
 import java.text.ParseException;
 import java.time.LocalDateTime;
 import java.util.Date;
 
+@Component
+@Comment("百度搜索扩展方法")
 public class BaiduExtendFunction implements FunctionExecutor {
 
     @Comment("将百度搜索结果中的时间转换为yyyy-MM-dd格式的字符串")
